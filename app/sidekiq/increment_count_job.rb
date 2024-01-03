88c2f508cde56775e84e631b03c6b33283b2f41e
Brandon Valle <bjvtamayo78@gmail.com>
2024-01-03 11:50:28 -0400

    Fix the typo of the post attribute

--- app/sidekiq/increment_count_job.rb
+++ app/sidekiq/increment_count_job.rb
@@ -4,8 +4,8 @@ class IncrementCountJob
   def perform(post_id)
     post = Post.find(post_id)
 
-    post.like_count ||= 0
-    post.like_count += 1
+    post.likes_count ||= 0
+    post.likes_count += 1
     post.save
   end
 end
